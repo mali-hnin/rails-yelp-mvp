e032cc9dfe1c73147fe3d12a7c639ea7c9898f9d
Mariasole Alossa <mariasole.alossa@gmail.com>
2017-08-09 15:52:54 +0100

    restaurants validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
 end
