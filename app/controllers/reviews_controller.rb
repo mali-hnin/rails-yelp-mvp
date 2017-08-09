b2390fd234d4027f07b0ba40070f03692cb23b72
Mariasole Alossa <mariasole.alossa@gmail.com>
2017-08-09 16:59:27 +0100

    reviews new and create methods and added form for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,17 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
   end
 
-  def destroy
-
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
